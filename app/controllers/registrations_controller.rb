c7258f97b4b52cf44db8aade1981ba4f6433282d
Afonso Neto <afonso.pontesneto@gmail.com>
2016-02-24 23:45:52 -0300

    Adds bootstrap header menu for signed in user.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,5 +3,7 @@ class RegistrationsController < Devise::RegistrationsController
 	before_action :authenticate_user!
 	
   def dashboard
+  	@members 	= User.members
+  	@polls 		= Poll.all
   end
 end 
