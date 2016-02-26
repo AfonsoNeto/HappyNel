0456d594fba9bbeba52c3e13f293861008e90192
Afonso Neto <afonsoneto@ffit.com.br>
2016-02-26 19:21:54 -0300

    Better layout on dashboard screen.

--- app/helpers/polls_helper.rb
+++ app/helpers/polls_helper.rb
@@ -1,2 +1,22 @@
 module PollsHelper
+	def render_stars_count(score)
+		score 			= score.round
+		total_stars = 5
+
+		html = ""
+
+		# Adds filled stars
+		for i in (1..score) do
+			html += content_tag :span, class: "glyphicon glyphicon-star" do
+			end
+		end
+
+		# Adds empty stars
+		for j in (1..(total_stars - score)) do
+			html += content_tag :span, class: "glyphicon glyphicon-star-empty" do
+			end
+		end
+
+		raw html
+	end
 end
